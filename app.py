36abfd68aa17f653d7cb9df6ba9c303487dbb17e
Daniel Palma Garcia <danpalma94@gmail.com>
2025-11-18 21:57:54 -0600

    added GET /planet_details and POST /add_planet endpoints

--- app.py
+++ app.py
@@ -151,6 +151,39 @@ def retrieve_password(email: str):
         return jsonify(message="Password sent to " + email), 200
     return jsonify(message="That email doesn't exist"), 401
 
+@app.route('/planet_details/<int:planet_id>', methods=['GET'])
+def planet_details(planet_id):
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        result = planet_schema.dump(planet)
+        return jsonify(result), 200
+    return jsonify(message='That planet does not exist!'), 404
+
+
+@app.route('/add_planet', methods=['POST'])
+@jwt_required()
+def add_planet():
+    planet_name = request.form['planet_name']
+    test = Planet.query.filter_by(planet_name=planet_name).first()
+    if test:
+        return jsonify(message="There is already a planet by that name"), 409
+    planet_type = request.form['planet_type']
+    home_star = request.form['home_star']
+    mass = float(request.form['mass'])
+    radius = float(request.form['radius'])
+    distance = float(request.form['distance'])
+
+    new_planet = Planet(planet_name=planet_name, 
+                        planet_type=planet_type,
+                        home_star=home_star,
+                        mass=mass,
+                        radius=radius,
+                        distance=distance)
+    db.session.add(new_planet)
+    db.session.commit()
+    return jsonify(message='You added a planet'), 201
+
+
 
 # ----- Database Models -----
 
